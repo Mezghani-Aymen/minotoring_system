698f99177ed5181f5e50d94291d70227949dcddc
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-01-25 21:51:22 +0100

    feat: add window monitor detection functionality

---
+++ monitor_detector.py
@@ -0,0 +1,18 @@
+import win32api
+import win32con
+from window_utils import get_foreground_hwnd
+
+def get_window_monitor():
+    hwnd = get_foreground_hwnd()
+    if not hwnd:
+        return None
+
+    monitor = win32api.MonitorFromWindow(
+        hwnd, win32con.MONITOR_DEFAULTTONEAREST
+    )
+    info = win32api.GetMonitorInfo(monitor)
+
+    return {
+        "device": info["Device"],
+        "primary": info["Flags"] == 1,
+    }
