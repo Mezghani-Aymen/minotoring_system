f153e9127d997ef20d3e8073aaee0f78101aad5e
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-02-08 18:36:16 +0100

    refactor: restructure Python backend folders

--- config.py
+++
@@ -1,5 +0,0 @@
-AFK_THRESHOLD_SECONDS = 60  # 1 minute
-INTERVAL = 1  # seconds
-RESULTS_FILE = "data.json"
-IDLE_THRESHOLD_SECONDS = 5
-INTERACTION_SCHEMA = ["date", "timestamp", "application", "context" , "topic"]
