5cad03e4278e6a01d836559b010afcec16c9a2c8
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-02-09 15:30:29 +0100

    fix: add AFK_ALERT_SHOWN variable to settings for tracking alert status

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -4,6 +4,8 @@ IDLE_THRESHOLD_SECONDS = 5
 INTERACTION_SCHEMA = ["date", "timestamp", "application", "context" , "topic"]
 OPTIMIZIED_ATTRIBUTES_SCHEMA = ["date", "total_time", "application", "context" , "monitor"]
 MERGED_ATTRIBUTES_SCHEMA = ["date", "total_time", "application", "context" , "monitor","topic","extra","primary"]
+AFK_ALERT_SHOWN = False
+
 # ATTRIBUTES_SCHEMA = {
 #     "date": "date",
 #     "context": "context",
