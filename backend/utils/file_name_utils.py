4ee5d73d4488505821c2711ccd4773618d3a7dd0
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-02-12 15:47:07 +0100

    fix: update get_filename function to use consistent date parameter

--- backend/utils/file_name_utils.py
+++ backend/utils/file_name_utils.py
@@ -1,6 +1,5 @@
-from utils.time_utils import current_date
 
-def get_filename(file_type , date_str) :
+def get_filename(file_type , date) :
     """
     Return dynamic file name based on type.
     
@@ -8,10 +7,10 @@ def get_filename(file_type , date_str) :
     """
     
     if file_type == "raw":
-        return f"raw_{date_str}.json"
+        return f"raw_{date}.json"
     elif file_type == "aggregated":
-        return f"aggregated_{date_str}.json"
+        return f"aggregated_{date}.json"
     elif file_type == "archive":
-        return f"archive_raw_{date_str}.json"
+        return f"archive_raw_{date}.json"
     else:
         raise ValueError(f"Unknown file type: {file_type}")
