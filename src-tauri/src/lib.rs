618cb935724a4174ab0262ba3a3315e007b6ba0d
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-02-15 19:52:31 +0100

    feat: update notification title and body in Tauri application

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,17 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+
+mod theme;
+
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_notification::init())
+        .invoke_handler(tauri::generate_handler![theme::switch_theme])
         .setup(|app| {
             use tauri_plugin_notification::NotificationExt;
             app.notification()
                 .builder()
-                .title("Tauri")
-                .body("Tauri is awesome")
+                .title("Monitoring System")
+                .body("The app is running")
                 .show()
                 .unwrap();
 
