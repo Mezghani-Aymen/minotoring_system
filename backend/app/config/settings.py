caae2f23f6498d69da393ed53d03e059aec8b4bb
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-03-10 15:09:48 +0100

    feat: update settings.py to define RAW_FILE_PATH and AGGREGATED_FILE_PATH

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -5,7 +5,8 @@ INTERACTION_SCHEMA = ["date", "timestamp", "application", "context" , "topic"]
 OPTIMIZIED_ATTRIBUTES_SCHEMA = ["date", "total_time", "application", "context" , "monitor"]
 MERGED_ATTRIBUTES_SCHEMA = ["date", "total_time", "application", "context" , "monitor","topic","extra","primary"]
 AFK_ALERT_SHOWN = False
-
+RAW_FILE_PATH = "./data/raw/"
+AGGREGATED_FILE_PATH = "./data/aggregated/"
 # ATTRIBUTES_SCHEMA = {
 #     "date": "date",
 #     "context": "context",
