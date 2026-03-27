237a6e7af4127e06c9ae8b528b8c06ec3c57a574
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-03-27 16:44:22 +0100

    feat: add get base path logic

---
+++ src-tauri/src/file_service.rs
@@ -0,0 +1,10 @@
+use tauri::{AppHandle, Manager};
+
+pub fn get_base_path(app: &AppHandle) -> Result<std::path::PathBuf, String> {
+    let is_dev = cfg!(debug_assertions);
+    if is_dev {
+        Ok(std::path::PathBuf::from("../backend/data/"))
+    } else {
+        app.path().app_data_dir().map_err(|e| e.to_string())
+    }
+}
