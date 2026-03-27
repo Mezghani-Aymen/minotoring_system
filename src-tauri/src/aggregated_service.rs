a2b2f5bcd08707208295eda05252242aa6b40db4
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-03-27 15:54:03 +0100

    feat: add fetch_aggregated_data function

---
+++ src-tauri/src/aggregated_service.rs
@@ -0,0 +1,15 @@
+#[tauri::command]
+pub fn fetch_aggregated_data(app: tauri::AppHandle, date: String) -> Result<String, String> {
+    use crate::python_commands;
+    use std::fs;
+
+    let base_path = python_commands::get_base_path(&app)?;
+    let mut path = base_path.join("aggregated");
+
+    path.push(format!("aggregated_{}.json", date));
+
+    match fs::read_to_string(&path) {
+        Ok(data) => Ok(data),
+        Err(e) => Err(format!("Error reading file at {:?}: {}", path, e)),
+    }
+}
