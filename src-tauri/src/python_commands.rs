8d3a032238b16772eb08820f689a9f831352fd5c
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-03-28 08:39:10 +0100

    feat: orchestrate python process lifecycle in run_python utils

--- src-tauri/src/python_commands.rs
+++ src-tauri/src/python_commands.rs
@@ -1,7 +1,9 @@
-use crate::process_utils::check_and_cleanup_process;
+use crate::process_utils::{
+    build_command, check_and_cleanup_process, spawn_process, store_process,
+};
 use std::process::Child;
 use std::sync::Mutex;
-use tauri::State;
+use tauri::{AppHandle, State};
 
 pub struct PythonState {
     pub child: Mutex<Option<Child>>, // Option<Child> ==> {Return: Some(process) → Python is running / None → Python is NOT running}
@@ -11,4 +13,40 @@ pub struct PythonState {
 pub fn is_python_running(state: State<'_, PythonState>) -> bool {
     let mut lock = state.child.lock().unwrap();
     check_and_cleanup_process(&mut *lock)
+}
+
+#[tauri::command]
+pub fn run_python(app: AppHandle, state: State<'_, PythonState>) -> Result<String, String> {
+    use crate::env_utils::set_envs;
+    use crate::file_utils::{ensure_directories, get_base_path, prepare_paths};
+
+    // 1. Check running
+    {
+        let mut lock = state.child.lock().unwrap();
+        if check_and_cleanup_process(&mut *lock) {
+            return Ok("Monitoring System Already Running".to_string());
+        }
+    }
+
+    // 2. Paths
+    let base_path = get_base_path(&app)?;
+    let (raw_path, aggregated_path) = prepare_paths(&base_path);
+
+    // 3. Ensure dirs
+    ensure_directories(&raw_path, &aggregated_path)?;
+
+    // 4. Build command
+    let is_dev = cfg!(debug_assertions);
+    let mut cmd = build_command(&app, is_dev)?;
+
+    // 5. Env variables
+    set_envs(&mut cmd, is_dev, &base_path, &raw_path, &aggregated_path);
+
+    // 6. Spawn
+    let child = spawn_process(&mut cmd)?;
+
+    // 7. Store
+    store_process(&state, child);
+
+    Ok("Monitoring System Started in Background".to_string())
 }
