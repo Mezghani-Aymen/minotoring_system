543b3d9f3242a726291bd53e36ccd60b70eee32f
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-03-28 16:27:17 +0100

    feat: register core service modules in application entry point

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,13 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-
+mod data_service;
+mod env_utils;
+mod file_utils;
+mod process_utils;
+mod python_commands;
 mod theme;
 
 pub fn run() {
+    // TODO: Add stop tracking when i close app.
     tauri::Builder::default()
         .plugin(tauri_plugin_notification::init())
         .invoke_handler(tauri::generate_handler![theme::switch_theme])
