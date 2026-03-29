ac9cf2d54370b96a8a48df4aafb4bb1d942652d6
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-03-30 00:42:40 +0200

    feat: implement graceful shutdown by stopping Python process on app exit

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,8 +6,11 @@ mod process_utils;
 mod python_commands;
 mod theme;
 
+use crate::process_utils::stop_python;
+use crate::python_commands::PythonState;
+use tauri::{Manager, RunEvent};
+
 pub fn run() {
-    // TODO: Add stop tracking when i close app.
     tauri::Builder::default()
         .plugin(tauri_plugin_notification::init())
         .invoke_handler(tauri::generate_handler![
@@ -28,15 +31,21 @@ pub fn run() {
                 .show()
                 .unwrap();
 
-            if cfg!(debug_assertions) {
-                app.handle().plugin(
-                    tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
-                        .build(),
-                )?;
-            }
+            app.handle().plugin(
+                tauri_plugin_log::Builder::default()
+                    .level(log::LevelFilter::Info)
+                    .build(),
+            )?;
             Ok(())
         })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application")
+        .run(|app_handle, event| match event {
+            RunEvent::ExitRequested { .. } | RunEvent::Exit => {
+                log::info!(">>> App exiting (handler: {:?})", event);
+                let state = app_handle.state::<PythonState>();
+                stop_python(app_handle, &state);
+            }
+            _ => {}
+        });
 }
