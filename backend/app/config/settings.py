de3e0e5310b4bb54dcc87d2ba18daefdccaaa1b7
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-03-29 16:59:01 +0200

    feat: refactor file path handling and ensure directories exist

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,3 +1,6 @@
+import os
+from pathlib  import Path
+
 AFK_THRESHOLD_SECONDS = 60  # 1 minute
 INTERVAL = 1  # seconds
 IDLE_THRESHOLD_SECONDS = 5
@@ -5,16 +8,9 @@ INTERACTION_SCHEMA = ["date", "timestamp", "application", "context" , "topic"]
 OPTIMIZIED_ATTRIBUTES_SCHEMA = ["date", "total_time", "application", "context" , "monitor"]
 MERGED_ATTRIBUTES_SCHEMA = ["date", "total_time", "application", "context" , "monitor","topic","extra","primary"]
 AFK_ALERT_SHOWN = False
-RAW_FILE_PATH = "./data/raw/"
-AGGREGATED_FILE_PATH = "./data/aggregated/"
-# ATTRIBUTES_SCHEMA = {
-#     "date": "date",
-#     "context": "context",
-#     "value": "value",
-#     "timestamp": "timestamp",
-#     "application": "application",
-#     "topic": "topic",
-#     "monitor": "monitor",
-#     "primary": "primary",
-#     "extra": "extra"
-# }
+RAW_FILE_PATH = Path(os.getenv("RAW_FILE_PATH", "./data/raw/"))
+AGGREGATED_FILE_PATH = Path(os.getenv("AGGREGATED_FILE_PATH", "./data/aggregated/"))
+
+# Ensure directories exist
+RAW_FILE_PATH.mkdir(parents=True, exist_ok=True)
+AGGREGATED_FILE_PATH.mkdir(parents=True, exist_ok=True)
