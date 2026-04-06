daf5c42084d701f1a190daa0845512e9cb791d70
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2026-04-06 20:19:07 +0100

    refactor: update set_envs function to return Command and improve chaining

--- src-tauri/src/env_utils.rs
+++ src-tauri/src/env_utils.rs
@@ -1,14 +1,15 @@
-use std::{path::PathBuf, process::Command};
+use std::path::PathBuf;
+use tauri_plugin_shell::process::Command;
 
 pub fn set_envs(
-    cmd: &mut Command,
+    cmd: Command,
     is_dev: bool,
     base: &PathBuf,
     raw: &PathBuf,
     aggregated: &PathBuf,
-) {
-    cmd.env("APP_MODE", if is_dev { "dev" } else { "prod" });
-    cmd.env("APP_DATA_DIR", base.to_string_lossy().to_string());
-    cmd.env("RAW_FILE_PATH", raw.to_string_lossy().to_string());
-    cmd.env("AGGREGATED_FILE_PATH", aggregated.to_string_lossy().to_string());
+) -> Command {
+    cmd.env("APP_MODE", if is_dev { "dev" } else { "prod" })
+        .env("APP_DATA_DIR", base.to_string_lossy().to_string())
+        .env("RAW_FILE_PATH", raw.to_string_lossy().to_string())
+        .env("AGGREGATED_FILE_PATH", aggregated.to_string_lossy().to_string())
 }
